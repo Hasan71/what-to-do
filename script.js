bef2febcef4cd74ff9a28c56a17a918099bbf176
Mahmudul Hasan <mahmud.cse7@gmail.com>
2021-08-29 20:57:58 +0600

    added DOM manipulation for button and lsts

---
+++ script.js
@@ -0,0 +1,41 @@
+//selectors
+
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+
+function addTodo(event){
+    event.preventDefault(); // preventing form from submitting
+
+    //todo div
+    const todoDiv  = document.createElement("div");
+    
+    //creating List
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    
+    //checked button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i> class="fas fa-check" </i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+
+    //checked Trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i> class="fas fa-trash" </i>';
+    trashButton.classList.add('complete-btn');
+    todoDiv.appendChild(trashButton);
+
+
+
+
+
+}
+
+
